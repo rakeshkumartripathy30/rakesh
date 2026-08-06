62969588bad4141c5d8e82150b14163c15a9d723
rakeshkumartripathy <rakeshkumartripathy1990@gmail.com>
2026-08-06 11:11:13 +0530

    Assignment5

---
+++ typescript-training/Assignments/Assignment5.ts
@@ -0,0 +1,55 @@
+interface employee {     //define employee structure
+    name: string;
+    baseSalary: number;
+    experience: number;
+    rating: number;
+}
+//store the employee records in an array
+let emp: employee[] = [
+    { name: "AliceJohnson", baseSalary: 75000.0, experience: 5.1, rating: 4.2 },
+    { name: "Bob Smith", baseSalary: 68000.0, experience: 3.2, rating: 3.8 },
+    { name: "Carol Davis", baseSalary: 82000.0, experience: 7.1, rating: 4.5 },
+    { name: "David Brown", baseSalary: 90000.0, experience: 10.2, rating: 2.5 },
+    { name: "Eva Green", baseSalary: 60000.0, experience: 2.4, rating: 3.5 }
+];
+//create the map to store result
+let hikeMap: Map<string, number> = new Map();
+//Loop through employees and calculate hike
+for (let emp of employee) {
+
+    let variablePayPercent: number;
+    let bonus: number;
+
+}
+// Conditional statements based on rating
+if (emp.rating >= 4.0) {
+    variablePayPercent = 15.0;
+    bonus = 1500;
+}
+else if (emp.rating >= 3.0 && < 4.0) {
+    variablePayPercent = 10.0;
+    bonus = 1200;
+}
+else {
+    variablePayPercent = 3.0;
+    bonus = 300;
+}
+//
+// Extra reward for experience >= 5 years
+let reward: number = emp.experience >= 5 ? 5000 : 0
+
+// // Hike calculation
+
+let hike: number = (emp.baseSalary * (variablePayPercent / 100)) + bonus + reward;
+let hikePercent: number = (hike / emp.baseSalary) * 100;
+
+    // Store result in Map
+hikeMap.set(emp.name , hikePercent);
+
+// Step 5: Print results
+for (let [name, hikePercent] of hikeMap) {
+    console.log(`${name} → Hike % = ${hikePercent.toFixed(2)}%`);
+}
+
+
+
